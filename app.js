fec9861aaa87ac615243479d51f83ed3d250bc36
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-02-25 12:29:38 +0600

    second

--- app.js
+++ app.js
@@ -1,50 +1,24 @@
-const person = {
-  found: 2,
-  message: "Found 2 persons",
-  result: [
-    {
-      name: {
-        common: "John",
-        fullName: ["John", "Doe"],
-      },
-      age: 32,
-      isMale: false,
-      address: {
-        street: "13/A St Joseph",
-        house: 10,
-      },
-    },
-    {
-      name: {
-        common: "Humayoun",
-        fullName: ["Humayoun", "Kabir"],
-      },
-      age: 33,
-      isMale: false,
-      address: {
-        street: "13/A St Lucia",
-        house: 11,
-      },
-    },
-  ],
-};
-function loadData(person){
-    console.log(person.result[0].address.street)
-    const cardContainer = document.getElementById('card-container')
-    const cardName = document.getElementById('name')
-    const cardName2 = document.getElementById('name2')
-    const age = document.getElementById('age');
-    const age2 = document.getElementById('age2');
-    const location = document.getElementById('location');
-    const location2 = document.getElementById('location2');
-    cardName.innerText =`Person Name: ${person.result[0].name.common}`
-    age.innerText = `age ${person.result[0].age}`
-    location.innerText = `Street: ${person.result[0].address.street}, House : ${person.result[0].address.house}`
 
-    // card 2
-    cardName2.innerText =`Person Name: ${person.result[1].name.common}`
-    age2.innerText = `age ${person.result[1].age}`
-    location2.innerText = `Street: ${person.result[1].address.street}, House : ${person.result[1].address.house}`
-
+function loadData(){
+    fetch("practice-1.json")
+    .then(res => res.json())
+    .then(data => displayData(data.result))
 }
-loadData(person)
+const displayData = (cards) => {
+  const cardContainer = document.getElementById('card-container')
+  cards.forEach(cardDetail => {
+    console.log(cardDetail.address.street)
+    const div = document.createElement('div');
+    div.innerHTML =` 
+    <div class="card border-success mb-3 shadow-lg p-2">
+          <h6 class="card-header bg-transparent border-success">Name: ${cardDetail.name.fullName[0]} ${cardDetail.name.fullName[1]}</h6>
+          <div class="card-body text-success">
+            <h5 class="card-title">age: ${cardDetail.age}</h5>
+            <p class="card-text">Street: ${cardDetail.address.street}, ${cardDetail.address.house}</p>
+        </div>
+    </div>
+    `
+    cardContainer.appendChild(div)
+  });
+}
+loadData()
